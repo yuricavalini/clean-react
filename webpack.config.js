d3db1c75e3b3ba7a473414a46696fdc1743f3d09
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-11 17:14:09 -0300

    chore: config webpack and add jsdom

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    fileName: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  devServer: {
+    static: './public',
+    historyApiFallBack: true,
+    devMiddleware: {
+      writeToDisk: true
+    }
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
