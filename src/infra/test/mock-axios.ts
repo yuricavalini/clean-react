9a00ed6cb6ba01e496d19d7a45e1d073f443e6af
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-11 15:33:18 -0300

    refactor: move axios mock to a separate file

---
+++ src/infra/test/mock-axios.ts
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker'
+import axios from 'axios'
+
+export const mockAxios = (): jest.Mocked<typeof axios> => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>
+  mockedAxios.post.mockResolvedValue({
+    data: {},
+    status: faker.random.numeric()
+  })
+  return mockedAxios
+}
