25f441e8f13f0245c4bdce14a33f26c8092401db
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-21 20:16:23 -0300

    feat: add login view

---
+++ src/main/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import Login from '@/presentation/login/login'
+
+// https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
+const container = document.getElementById('main')
+const root = createRoot(container!) // createRoot(container!) if you use TypeScript
+root.render(<Login />)
---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Styles from './spinner-styles.scss'
+
+type Props = React.HTMLAttributes<HTMLElement>
+
+/* eslint-disable react/prop-types */
+const Spinner: React.FC<Props> = (props: Props) => {
+  return (
+    <div {...props} className={[Styles.spinner, props.className].join(' ')}>
+      <div /><div /><div /><div />
+    </div>
+  )
+}
+
+export default Spinner
