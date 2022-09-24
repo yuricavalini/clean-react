25ce9de80055a1d4c119ab615e0dc6462bab3d40
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-24 08:35:35 -0300

    refactor: change test to the recommended way of testing-library/react

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -10,7 +10,7 @@ const FormStatus: React.FC = () => {
   const { isLoading, errorMessage } = useContext(Context) as LoginState
 
   return (
-    <div data-testid="error-wrap" className={Styles.errorWrap}>
+    <div aria-label="error-wrap" className={Styles.errorWrap}>
       { isLoading && <Spinner className={Styles.spinner} />}
       { errorMessage && <span className={Styles.error}>{errorMessage}</span> }
     </div>
--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Login from './login'
 
 describe('Login Component', () => {
-  test('Should not render spinner and error on start', () => {
-    const { getByTestId } = render(<Login />)
-    const errorWrap = getByTestId('error-wrap')
+  test('Should start with initial state', () => {
+    render(<Login />)
+    const errorWrap = screen.getByRole('generic', { name: 'error-wrap' })
     expect(errorWrap.childElementCount).toBe(0)
   })
 })
