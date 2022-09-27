718f3ec0be621a51f3d5ec5145acf9270eefb555
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-27 06:32:44 -0300

    refactor: move ValidationSpy to its own file

---
+++ src/presentation/test/mock-validation.ts
@@ -0,0 +1,13 @@
+import { Validation } from '@/presentation/protocols/validation'
+
+export class ValidationSpy implements Validation {
+  errorMessage = ''
+  fieldName = ''
+  fieldValue = ''
+
+  validate (fieldName: string, fieldValue: string): string {
+    this.fieldName = fieldName
+    this.fieldValue = fieldValue
+    return this.errorMessage
+  }
+}
