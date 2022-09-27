a6bfc3bf5433b29d6d190ddbfb39f61077db95df
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-27 06:21:27 -0300

    feat: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -10,6 +10,7 @@ import { Validation } from '@/presentation/protocols/validation'
 export interface LoginState {
   isLoading: boolean
   email: string
+  password: string
   emailError: string
   passwordError: string
   mainError: string
@@ -27,6 +28,7 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   const [state, setState] = useState<LoginState>({
     isLoading: false,
     email: '',
+    password: '',
     emailError: 'Campo obrigatório',
     passwordError: 'Campo obrigatório',
     mainError: ''
@@ -36,14 +38,18 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     validation.validate({ email: state.email })
   }, [state.email])
 
+  useEffect(() => {
+    validation.validate({ password: state.password })
+  }, [state.password])
+
   return (
     <div className={Styles.login}>
       <LoginHeader />
       <Context.Provider value={{ state, setState }}>
         <form className={Styles.form}>
           <h2>Login</h2>
-          <Input type="email" name="email" placeholder='Digite seu e-mail' />
-          <Input type="password" name="password" placeholder='Digite sua senha' />
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input type="password" name="password" placeholder="Digite sua senha" />
           <button className={Styles.submit} disabled type="submit">Entrar</button>
           <span className={Styles.link}>Criar conta</span>
           <FormStatus />
