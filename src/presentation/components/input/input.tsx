85742843ba06470f39aaa9273cc2c8ede0be03a4
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-28 05:39:43 -0300

    feat: ensure Login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -22,10 +22,10 @@ const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo certo!'
   }
 
   return (
