e502def07797419a51a761767642fecb77e8d458
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-28 09:10:28 -0300

    feat: ensure Login calls Authentication with correct values

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -6,6 +6,7 @@ import { Footer, Input, FormStatus, LoginHeader } from '@/presentation/component
 
 import Context from '@/presentation/contexts/form/login-context'
 import { Validation } from '@/presentation/protocols/validation'
+import { Authentication } from '@/domain/usecases'
 
 export interface LoginState {
   isLoading: boolean
@@ -22,9 +23,10 @@ export interface LoginStateContext {
 
 type Props = {
   validation: Validation
+  authentication: Authentication
 }
 
-const Login: React.FC<Props> = ({ validation }: Props) => {
+const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const [state, setState] = useState<LoginState>({
     isLoading: false,
     email: '',
@@ -41,17 +43,22 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
         emailError: validation.validate('email', state.email),
         passwordError: validation.validate('password', state.password)
       }))
-  }, [state.email, state.password])
+  }, [state.email, state.password, validation])
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
     setState((prevState) => ({ ...prevState, isLoading: true }))
+    await authentication.auth({
+      email: state.email,
+      password: state.password
+    })
   }
 
   return (
     <div className={Styles.login}>
       <LoginHeader />
       <Context.Provider value={{ state, setState }}>
+        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
         <form className={Styles.form} onSubmit={handleSubmit}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
