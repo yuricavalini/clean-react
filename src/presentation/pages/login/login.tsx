e6ca72f869dfff462efcca84d69c73a4e73ee565
Yuri Cavalini <yuricavalini@gmail.com>
2022-09-29 08:01:09 -0300

    feat: prevent Login to call submit more than once

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -47,6 +47,9 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
+    if (state.isLoading) {
+      return
+    }
     setState((prevState) => ({ ...prevState, isLoading: true }))
     await authentication.auth({
       email: state.email,
