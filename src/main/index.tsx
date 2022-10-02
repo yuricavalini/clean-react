84189379257029a216225891697e3ba7ed9a4690
Yuri Cavalini <yuricavalini@gmail.com>
2022-10-02 16:54:18 -0300

    feat: add Login composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Router } from '@/presentation/components'
 import '@/presentation/styles/global.scss'
+import { makeLogin } from './factories/pages/login/login-factory'
 
 // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
 const container = document.getElementById('main')
 const root = createRoot(container!) // createRoot(container!) if you use TypeScript
-root.render(<Router />)
+root.render(
+  <Router
+    makeLogin={makeLogin}
+  />
+)
